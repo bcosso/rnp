83a349547296340fc214c281040a8672e9073ff0
henrico <bcosso@gmail.com>
2025-10-30 19:19:16 +0100

    Creation of select method

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -1,39 +1,36 @@
-use std::fmt;
 use crate::configs;
 use crate::configs::Peer;
-use serde::{Deserialize, Serialize};
 use rsocket_rust::prelude::*;
-use rsocket_rust::Result;
 use rsocket_rust::Client;
+use rsocket_rust::Result;
 use rsocket_rust_transport_tcp::TcpClientTransport;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fmt;
 
-
-pub struct ConnManager{
+pub struct ConnManager {
     pub connections: HashMap<String, Client>,
 }
 
 impl ConnManager {
     pub fn new() -> Self {
-        ConnManager{connections : HashMap::new(),}
+        ConnManager {
+            connections: HashMap::new(),
+        }
     }
 
-    pub async fn init_connections(&mut self, peers :Vec<Peer>) -> Result<String>{
-
-        for item in peers {
-            self.create_client(item).await?;
-        }
+    pub async fn init_connections(&mut self, peers: Peer) -> Result<String> {
+        self.create_client(peers).await?;
 
         Ok(String::from("Ok"))
     }
 
-    
-    pub async fn create_client(&mut self, peer: configs::Peer) -> Result<String>{
+    pub async fn create_client(&mut self, peer: configs::Peer) -> Result<String> {
         let name_peer = peer.name;
         let port_peer = peer.port;
         let host_peer = peer.ip;
         let host_server = format!("{host_peer}:{port_peer}");
-        
+
         let cli = RSocketFactory::connect()
             .transport(TcpClientTransport::from(host_server))
             .setup(Payload::from("READY!"))
@@ -43,21 +40,15 @@ impl ConnManager {
             .await?;
         self.connections.insert(name_peer.to_string(), cli);
         Ok(String::from("Ok"))
-        
+
         //add cli to the list
     }
 }
 
-
-pub async fn create_instance(peer: Vec<Peer>) -> Result<ConnManager> {
+pub async fn create_instance(peer: Peer) -> Result<ConnManager> {
     let mut conn = ConnManager::new();
 
     conn.init_connections(peer).await;
 
     Ok(conn)
 }
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,112 @@
-use std::{fs::read, io::{stdin, stdout, Write}};
+use rsocket_rust::{prelude::*, Error};
+use std::{
+    fs::read,
+    io::{stdin, stdout, Write},
+};
 
 mod configs;
 mod connection_manager;
 
-fn main() {
-    while true{
+#[tokio::main]
+async fn main() {
+    let peers = configs::read_config_file().unwrap();
+    let conn = connection_manager::create_instance(peers[0].clone())
+        .await
+        .unwrap();
+
+    while true {
         let mut command = String::new();
         print!("nimpha> ");
-        let _=stdout().flush();
+        let _ = stdout().flush();
         stdin().read_line(&mut command);
         print!("{}", command);
         let args = command.split(" ").map(|s| s.to_string()).collect();
-        parse(args);
+        parse(args, command, &conn, peers[0].clone());
     }
 }
 
-fn parse(tokens: Vec<String>){
-    for token in tokens{
-        print!("{}\n", token);
+fn parse(
+    tokens: Vec<String>,
+    query: String,
+    conn: &connection_manager::ConnManager,
+    peer: configs::Peer,
+) {
+    if tokens[0] == "command" {
+        executeCommand(tokens, peer, conn);
+    }
+    //for token in tokens{
+    //    print!("{}\n", token);
+    //}
+}
+
+fn executeQuery(query: String, peer: configs::Peer, conn: &connection_manager::ConnManager) {}
+
+async fn executeCommand(
+    tokens: Vec<String>,
+    peer: configs::Peer,
+    conn: &connection_manager::ConnManager,
+) {
+    match tokens[1].as_str() {
+        "select" => executeSelect(tokens, peer, conn).await,
+        _ => (),
+    }
+}
+
+async fn executeSelect(
+    tokens: Vec<String>,
+    peer: configs::Peer,
+    conn: &connection_manager::ConnManager,
+) {
+    let table = tokens[2].clone();
+    let fieldkey = tokens[3].clone();
+    let fieldvalue = tokens[4].clone();
+    let payload = "{{
+\"table\": \"{table}\",
+\"where_field\": \"{fieldkey}\",
+\"where_content\": \"{fieldvalue}\" 
+}}";
+
+    execute_in_cluster(
+        "select_data_where_worker_equals_rsocket",
+        payload,
+        peer,
+        conn,
+    )
+    .await;
+}
+
+async fn execute_in_cluster(
+    name_method: &str,
+    data_json: &str,
+    peer: configs::Peer,
+    conn: &connection_manager::ConnManager,
+) -> Result<String, Error> {
+    let name_peer = peer.name;
+    let port_peer = peer.port;
+    let host_peer = peer.ip;
+
+    // let cli = RSocketFactory::connect()
+    //     .transport(TcpClientTransport::from(host_server))
+    //     .setup(Payload::from("READY!"))
+    //     .mime_type("text/plain", "text/plain")
+    //     .on_close(Box::new(|| println!("connection closed")))
+    //     .start()
+    //     .await?;
+    //let conn = connections.lock().unwrap();
+    if let Some(cli) = conn.connections.get(&name_peer) {
+        let method = "{\"method\":\"execute_something\"}";
+        let data =
+            format!("{{\"method\":\"/{name_peer}/{name_method}\",\"payload\":{data_json}\"}}");
+        let req = Payload::builder()
+            .set_data_utf8(&data)
+            .set_metadata_utf8(method)
+            .build();
+        let res = cli.request_response(req).await?;
+        //println!("got: {:?}", res);
+        let result2 = res.unwrap();
+        let result1 = result2.data_utf8().unwrap();
+        Ok(String::from(result1))
+    } else {
+        Ok("No connection found".to_string())
     }
 }
